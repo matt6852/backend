940306210412dcf9d6ebaf3664ae780d035e49de
Сергей Мячин <s.myachin@Maksims-MacBook-Air-2.local>
2022-03-29 20:55:26 +0300

    set statuses and errod handler

--- 01/src/index.ts
+++ 01/src/index.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 const getBody = bodyParser.json();
 const port = 5010;
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 let videos = [
   { id: 1, title: "About JS - 01", author: "it-incubator.eu" },
   { id: 2, title: "About JS - 02", author: "it-incubator.eu" },
@@ -17,42 +17,57 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello: World!!!");
 });
 app.get("/videos", (req, res) => {
-  res.send(videos);
+  res.status(200).json({ success: true, data: videos });
 });
 app.post("/videos", (req: Request, res: Response) => {
-  const newVideo = {
-    id: +new Date(),
-    title: req.body.title,
-    author: "it-incubator.eu",
-  };
-  videos.push(newVideo);
-  res.status(201).send(newVideo);
+  if (req.body.title) {
+    const newVideo = {
+      id: +new Date(),
+      title: req.body.title,
+      author: "it-incubator.eu",
+    };
+    videos.push(newVideo);
+    res.status(201).json(newVideo);
+  } else {
+    res.status(400).json({ success: false, msg: "please provide value" });
+  }
 });
 app.get("/videos/:id", (req, res) => {
-  // res.send(videos);
   const { id } = req.params;
-  // console.log(id);
-
-  res.send(videos.filter((v) => v.id === +id));
+  if (id) {
+    return res.send(videos.filter((v) => v.id === +id));
+  } else {
+    return res.status(404).json({ success: false, msg: "please provide id" });
+  }
 });
 app.delete("/videos/:id", (req: Request, res: Response) => {
   // put your code here
   const { id } = req.params;
   // console.log(id);
-  videos = videos.filter((v) => v.id !== +id);
-  res.send(videos).status(204);
+
+  if (id) {
+    videos = videos.filter((v) => v.id !== +id);
+    return res.send(videos).status(200);
+  } else {
+    return res
+      .status(400)
+      .json({ success: false, msg: `no items with id ${req.params.id}` });
+  }
 });
 app.put("/videos/:id", (req: Request, res: Response) => {
   // put your code here
   const title = req.body.title;
   const { id } = req.params;
-  if (videos.find((v) => v.id === +id)) {
+  if (videos.find((v) => v.id === +id) && title) {
     videos = videos.map((v) => (v.id === +id ? { ...v, title } : v));
-    res.send(videos).status(201);
+    return res.send(videos).status(201);
   } else {
-    res.send(404);
+    return res.status(404).json({
+      success: false,
+      msg: (!title && "no title") || (!id && "no id"),
+    });
   }
 });
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
